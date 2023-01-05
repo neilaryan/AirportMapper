90fa122d43b274a7f463557bdd6bf09673ef00c2
neilaryan <106613335+neilaryan@users.noreply.github.com>
2023-01-05 15:42:09 +0000

    first commit

---
+++ AirportMapper.java/GraphLoader.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GraphLoader {
+  private graph<String, Double> airportGraph;
+  public GraphLoader(String filepathToDot) throws FileNotFoundException {
+    airportGraph = new graph<>();
+    new Scanner(System.in);
+  }
+
+  public graph<String, Double> loadGraph(String filepathToDot) throws FileNotFoundException {
+    Scanner scnr = new Scanner(new File(filepathToDot));
+    new ArrayList<>();
+    while (scnr.hasNextLine()) {
+      String line = scnr.nextLine();
+      Pattern pattern = Pattern.compile("\"(.*?)\"");
+      Matcher matcher = pattern.matcher(line);
+      String vertex = null;
+      if (matcher.find()) {
+        vertex = matcher.group();
+        airportGraph.insertVertex(vertex);
+      }
+      if (matcher.find()) {
+        String vertex2 = matcher.group();
+        airportGraph.insertVertex(vertex2);
+        airportGraph.insertEdge(vertex, vertex2,
+            Double.valueOf(line.substring(line.lastIndexOf(" ")+1, line.lastIndexOf("]"))));
+        airportGraph.insertEdge(vertex2, vertex,
+            Double.valueOf(line.substring(line.lastIndexOf(" ")+1, line.lastIndexOf("]"))));
+      }
+    }
+    return airportGraph;
+  }
+  public static void main(String[] args) throws FileNotFoundException {
+
+    GraphLoader newLoader = new GraphLoader("/workspaces/codespaces-blank/GraphLoader.java/graph.dot");
+    newLoader.loadGraph("/workspaces/codespaces-blank/GraphLoader.java/graph.dot");
+  }
+}
